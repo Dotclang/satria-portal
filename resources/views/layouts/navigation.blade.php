1fbdb90bff070143d2b5ab9292c9b1a21086bf99
Dotclang <edyridwanh@gmail.com>
2023-04-13 08:28:09 +0700

    update navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,7 +44,7 @@
                         </x-dropdown-link>
 
                         <a href="http://ems.test:8081/authenticate?email={{auth()->user()->email}}&token={{base64_encode(Hash::make(config('auth.token')))}}"
-                            class='block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out'>UTPE</a>
+                            class='block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out'>EMS Portal</a>
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
